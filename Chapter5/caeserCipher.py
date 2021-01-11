a1c2488481612ddbcd8c8f112bcca606cb0ba5cc
aisikawaa <alterthemadness@outlook.com>
2021-01-11 16:20:29 +0900

    update

--- Chapter5/caeserCipher.py
+++ Chapter5/caeserCipher.py
@@ -8,36 +8,48 @@ BLUE = Fore.BLUE
 RED = Fore.RED
 RESET = Fore.RESET
 
-parser = argparse.ArgumentParser(description="encrypt and decrypt of caesar cipher.")
-parser.add_argument("-m", dest="message", type=str)
-parser.add_argument("-k", dest="key", help="Amount of alphabetic shift.", type=int)
-parser.add_argument("-M", dest="mode", choices=["encrypt", "decrypt"])
 
-args = parser.parse_args()
-message = args.message
-key = args.key
-mode = args.mode
+def main():
+    parser = argparse.ArgumentParser(
+        description="encrypt and decrypt of caesar cipher."
+    )
+    parser.add_argument("-m", dest="message", type=str)
+    parser.add_argument("-k", dest="key", help="Amount of alphabetic shift.", type=int)
+    parser.add_argument("-M", dest="mode", choices=["encrypt", "decrypt"])
 
-SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?."
-translated = ""
+    args = parser.parse_args()
+    message = args.message
+    key = args.key
+    mode = args.mode
 
-for symbol in message:
-    if symbol in SYMBOLS:
-        symbol_index = SYMBOLS.find(symbol)
+    caeser(key=key, message=message, mode=mode)
 
-        if mode == "encrypt":
-            translated_index = symbol_index + key
-        elif mode == "decrypt":
-            translated_index = symbol_index - key
 
-        if translated_index >= len(SYMBOLS):
-            translated_index = translated_index - len(SYMBOLS)
-        elif translated_index < 0:
-            translated_index = translated_index + len(SYMBOLS)
-        translated = translated + SYMBOLS[translated_index]
+def caeser(key, message, mode):
+    SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?."
+    translated = ""
 
-    else:
-        translated = translated + symbol
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symbol_index = SYMBOLS.find(symbol)
 
-print(f"{BLUE}{translated}{RESET}")
-pyperclip.copy(translated)
+            if mode == "encrypt":
+                translated_index = symbol_index + key
+            elif mode == "decrypt":
+                translated_index = symbol_index - key
+
+            if translated_index >= len(SYMBOLS):
+                translated_index = translated_index - len(SYMBOLS)
+            elif translated_index < 0:
+                translated_index = translated_index + len(SYMBOLS)
+            translated = translated + SYMBOLS[translated_index]
+
+        else:
+            translated = translated + symbol
+
+    print(f"{BLUE}{translated}{RESET}")
+    pyperclip.copy(translated)
+
+
+if __name__ == "__main__":
+    main()
